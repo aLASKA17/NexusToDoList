5f81b5dcdfaac10794bb12c3755acc6c56aa6302
a1aska17 <92105945+aLASKA17@users.noreply.github.com>
2024-07-01 18:12:50 +0600

    Add TAsk

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,9 @@ public class Main {
             @Override
             public void run() {
                 new MainWindow();
+
             }
+
         });
     }
 }
--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,30 +1,57 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainWindow {
+    JButton addButton,clearButton;
+    JPanel taskPanel,buttonsPanel;
+    JFrame mainWindow;
+    int idCounter = 1;
     public MainWindow(){
 
-        JFrame mainWindow = new JFrame("Nexus");
+        mainWindow = new JFrame("Nexus");
         mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// Cоздание окна приложения
         mainWindow.setSize(500,1000);
 
+        taskPanel = new JPanel();
+        taskPanel.setPreferredSize(new Dimension(500,800));// Создание панели для размешения задач
 
-        JPanel taskPanel = new JPanel();
-        taskPanel.setPreferredSize(new Dimension(500,800));
+        buttonsPanel = new JPanel();
+        buttonsPanel.setSize(new Dimension(500,200));// Создание панели для двух кнопок
 
-
-        JPanel buttonsPanel = new JPanel();
-        buttonsPanel.setSize(new Dimension(500,200));
-
-
-        JButton addButton = new JButton("Добавить задачу");
-        JButton clearButton = new JButton("Удалить решенные задачи");
+        addButton = new JButton("Добавить задачу");// Кнопка добавление и очистки задач
+        clearButton = new JButton("Удалить решенные задачи");
         buttonsPanel.add(addButton);
         buttonsPanel.add(clearButton);
 
-        mainWindow.getContentPane().add(BorderLayout.SOUTH,buttonsPanel);
+        mainWindow.getContentPane().add(BorderLayout.SOUTH,buttonsPanel);// Добавление панелей в окно
         mainWindow.getContentPane().add(BorderLayout.NORTH,taskPanel);
 
         mainWindow.setVisible(true);
+
+        addButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                AddTask();
+            }
+        });
+    }
+
+    public void AddTask(){
+        JLabel idTask = new JLabel(String.valueOf(idCounter));
+        idCounter++;
+
+        JTextField textTask = new JTextField();
+        textTask.setPreferredSize(new Dimension(400,30));
+
+        JCheckBox isDecide = new JCheckBox();
+
+        taskPanel.add(idTask);
+        taskPanel.add(textTask);
+        taskPanel.add(isDecide);
+
+        mainWindow.revalidate();
+
     }
 }
